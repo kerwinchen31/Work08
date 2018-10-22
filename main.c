90230cfb21aa1d79105fba00835a61c4c101c1a4
kerwinchen31 <kchen37@stuy.edu>
2018-10-21 22:54:57 -0400

    added files

--- main.c
+++ main.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
+
 #include <stdlib.h>
-#include <math.h>
+
 #include "sieve.h"
 
-int main(){
-  printf("\n answer %d \n", sieve(1000000));
+
+
+int main(int argc, char * argv[]){
+
+  int iterations = 1;
+
+  int target = 1000;
+
+  if(argc > 1){
+
+    target = atoi(argv[1]);
+
+  }
+
+  if(argc > 2){
+
+    iterations = atoi(argv[2]);
+
+  }
+
+
+
+
+
+  int ans = 0;
+
+  while(iterations>0){
+
+    ans=sieve(target);
+
+    iterations--;
+    printf("1k prime takes about 0.11s, 10k prime takes about 8.879 seconds, 1 millionth takes too long..... :( \n");
+    printf("The n=%d prime is %d\n", target, ans );
+
+
+
+    //this is to modify which prime to
+
+    //potentially avoid CPU caching
+
+    target++;
+
+
+
+  }
+
   return 0;
+
+
+
 }
--- sieve.c
+++ sieve.c
@@ -2,41 +2,19 @@
 #include <stdlib.h>
 #include <math.h>
 #include "sieve.h"
-/*
+
 int sieve(int n){
   int a = log(n) * n * 1.15;
-  int *array[a];
-  int i, j, k;
-  j = 2;
-  for(i = 0; i <= a; i++){
-    array[i] = 1;
-  }
-  array[a+1] = 0;
-  while (array[j]){
-    for (k = j; array[k] != 0; k++){
-      if (!(*array[k] % j)){
-	array[k] = 2;
-      }
-    }
-    j++;
-    while(array[j] == 2){
-      j++;
-    }
-  }
-  while(array[j] != 1){
-    j--;
-  }
-  return j;
-}
-*/
-int sieve(int n){
-  int a = log(n) * n * 1.15;
-  int *p = malloc(a * sizeof(int));
-  int *temp = p;
+  char *p = malloc(a * sizeof(char));
+  char *temp = p;
   int j = 2;
   int k, i;
   for(i = 0; i < a; i++){
+    if (!(i % 2 || i % 3)){
+      p[i] = 2;
+    }else{
     p[i] = 1;
+    }
     //printf("%d ", *p);
   }
   p[a] = 0;
@@ -65,6 +43,7 @@ int sieve(int n){
   while(p[j] != 3){
     j--;
     }
+  free(p);
   return j;
    
 }
